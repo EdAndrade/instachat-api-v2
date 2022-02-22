539b6dd25c0fd3029efb68232adc50302dea21da
edAndrade <edmilisonwork@gmail.com>
2022-02-22 17:02:34 +0100

    created doc swagger, created entity chat

--- src/instachat.api/Server.js
+++ src/instachat.api/Server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const chatRoutes = require('./Routes/ChatRoutes');
 const app = express();
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocs = require('./Routes/SwaggerDoc.json');
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/chatroom', chatRoutes)
+app.use('/api/chatroom', chatRoutes);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 module.exports = app;
--- src/instachat.api/Startup.js
+++ src/instachat.api/Startup.js
@@ -1,5 +1,5 @@
 const server = require('./Server');
 
 server.listen(8085, () => {
-    console.log('SERVER RUNNING');
+    console.log('SERVER RUNNING ================= ');
 });
