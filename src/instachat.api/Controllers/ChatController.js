f4aa25fa1f26f2759e3db41bad958681fda5f463
edAndrade <edmilisonwork@gmail.com>
2022-03-04 13:36:44 +0100

    created docker files

--- src/instachat.api/Controllers/ChatController.js
+++ src/instachat.api/Controllers/ChatController.js
@@ -5,10 +5,16 @@ class ChatController {
     constructor(){
         this._repository = new repository()
         this.createChat = this.createChat.bind(this)
+        this.getChat = this.createChat.bind(this)
     }
 
     createChat(request, response){
         this._repository.createChatRoom()
+        return response.sendStatus(200)
+    }
+
+    getChat(request, response){
+        this._repository.getChatRoom()
         return response.sendStatus(200)
     }
 }
--- src/instachat.api/Routes/ChatRoutes.js
+++ src/instachat.api/Routes/ChatRoutes.js
@@ -1,7 +1,9 @@
-const { Router } = require('express');
-const ChatController = require('../Controllers/ChatController');
+const { Router } = require('express')
+const ChatController = require('../Controllers/ChatController')
 
-const router = Router();
-router.post('/create_chat', ChatController.createChat);
+const router = Router()
 
-module.exports = router;
+router.post('/create_chat', ChatController.createChat)
+router.get('/get_chat', ChatController.getChat)
+
+module.exports = router
