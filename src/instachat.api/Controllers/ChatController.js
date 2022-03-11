085789abff1c57fd17e1abf3838f7eb5d15615a4
edAndrade <edmilisonwork@gmail.com>
2022-03-11 08:37:01 +0100

    getting chatRoom details

--- src/instachat.api/Controllers/ChatController.js
+++ src/instachat.api/Controllers/ChatController.js
@@ -15,27 +15,34 @@ class ChatController {
 
         const chatRequest = new ChatRequest(request.body)
 
-        if(chatRequest.isValid().valid){
+        try{
 
-            const chatRoom = new ChatRoom({
-                usersQt: chatRequest.peopleQtd,
-                name: chatRequest.chatName,
-                code: GenerateHash({ stringToEncode: `${new Date()}` })
-            })
+            if(chatRequest.isValid().valid){
 
-            const { code, name, usersQt } = await this._repository.createChatRoom(chatRoom)
-
-            return response.status(200).json({
-                code, name, usersQt
-            })
-
-        }else{
-            response.sendStatus(400)
+                const chatRoom = new ChatRoom({
+                    usersQt: chatRequest.peopleQtd,
+                    name: chatRequest.chatName,
+                    code: GenerateHash({ stringToEncode: `${new Date()}` })
+                })
+    
+                const { code, name, usersQt } = await this._repository.createChatRoom(chatRoom)
+    
+                return response.status(200).json({
+                    code, name, usersQt
+                })
+    
+            }else{
+                response.sendStatus(400)
+            }
+            
+        }catch(error){
+            return response.sendStatus(500)
         }
     }
 
-    getChat(request, response){
-        const data = this._repository.getChatRoom()
+    async getChat(request, response){
+        const code = request.body.code
+        const data = await this._repository.getChatRoom(code)
         return response.status(200).json(data)
     }
 }
