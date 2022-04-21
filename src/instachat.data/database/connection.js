be876d3dc072142d7df86f31dccd396471e2189a
edAndrade <edmilisonwork@gmail.com>
2022-04-21 12:44:46 +0100

    handled mongo promise

--- src/instachat.data/database/connection.js
+++ src/instachat.data/database/connection.js
@@ -2,7 +2,11 @@ require('dotenv').config()
 const DBConnection = require('mongoose')
 
 try{
-    DBConnection.connect(process.env.CONNECTION_STRING)
+    DBConnection.connect(process.env.CONNECTION_STRING).then( () => {
+        console.log('connected to mongo')
+    }).catch( (error) => {
+        console.log('couldnt connect to mongo', error) 
+    })
 }catch(error){
     console.log(error)
 }
